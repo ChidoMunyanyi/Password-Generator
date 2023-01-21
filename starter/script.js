a2496bef03ad2d41bd2020d346d792bbf9b6985d
ChidoMunyanyi <119809704+ChidoMunyanyi@users.noreply.github.com>
2023-01-21 20:19:29 +0000

    Added JavaScript Code to Generate Password

--- starter/script.js
+++ starter/script.js
@@ -88,19 +88,66 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var passwordLength = " ";
+var specialCharacters;
+var upperCasedCharacters;
+var lowerCasedCharacters;
+var numericCharacters;
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+var passwordLength = (prompt("How many characters would you like your password to contain?"));
+ 
+for(passwordLength <=9 || passwordLength >= 65) {
+  alert("Try Again! Password length must be between 10-64.");
+  var passwordLength = (prompt("How many characters would you like your password to contain?"));
 }
+}
+alert("Your password will have" + passwordLength + "characters")
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  
+ var specialCharacters = confirm("Click OK to confirm you would like to include special characters");
+ var upperCasedCharacters = confirm("Click OK to confirm you would like to include upper cased characters");
+ var lowerCasedCharacters =confirm("Click OK to confirm you would like to include lower cased characters");
+ var numericCharacters = confirm("Click OK to confirm you would like to include numeric characters");
+
+ for(specialCharacters === false && upperCasedCharacters === false && lowerCasedCharacters === false && numericCharacters === false) {
+  alert("You must have atleast one character type!");
+ var specialCharacters = confirm("Click OK to confirm you would like to include special characters");
+ var upperCasedCharacters = confirm("Click OK to confirm you would like to include upper cased characters");
+ var lowerCasedCharacters =confirm("Click OK to confirm you would like to include lower cased characters");
+ var numericCharacters = confirm("Click OK to confirm you would like to include numeric characters");
+ }
 
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  var passwordCharacters = []
 
+  if (specialCharacters) {
+    passwordCharacters = passwordCharacters.concat(specialCharacter)
+  }
+  if (upperCasedCharacters) {
+    passwordCharacters = passwordCharacters.concat(uppercaseCharacter)
+  }
+  if (lowerCasedCharacters) {
+    passwordCharacters = passwordCharacters.concat(lowercaseCharacter)
+  }
+  if (numericCharacters) {
+    passwordCharacters = passwordCharacters.concat(numericCharacter)
+  }
+ console.log(passwordCharacters)
+
+ var randomPassword = " "
+ 
+ for( var i = 0; i < passwordLength; i++) {
+  randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  console.log(passwordCharacters)
+ }
+ return randomPassword;
 }
 
 // Get references to the #generate element
